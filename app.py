36d460dddf29180bf77c9cefaf2f8d03c7e3f204
Portal Admin <portal@example.com>
2026-07-06 16:01:30 +0900

    Narrow the gap between the filename, Data Confirm, and Clear buttons by using gap='small' and adjusting column ratios

--- app.py
+++ app.py
@@ -826,8 +826,8 @@ def main():
     uploaded_file = st.session_state.saved_file
 
     if uploaded_file is not None:
-        # 2行目：ファイル名、データ確認ボタン、クリアボタンを並べる
-        col_file, col_btn, col_clear, _ = st.columns([1.5, 0.6, 0.6, 1.3], vertical_alignment="center")
+        # 2行目：ファイル名、データ確認ボタン、クリアボタンを隙間なく並べる
+        col_file, col_btn, col_clear, _ = st.columns([1.0, 0.45, 0.45, 2.1], gap="small", vertical_alignment="center")
         
         with col_file:
             excel_icon_svg = '''<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="16px" height="16px" style="vertical-align: middle; margin-right: 5px; margin-top: -2px;"><path fill="#4CAF50" d="M41,10H25v28h16c0.553,0,1-0.447,1-1V11C42,10.447,41.553,10,41,10z"/><path fill="#FFF" d="M32,15h2v18h-2V15z"/><path fill="#4CAF50" d="M27.5,10.5l-15-3v33l15-3V10.5z"/><path fill="#FFF" d="M19.141,29.35l2.253-3.666l-2.029-3.957h2.646l1.085,2.449c0.165,0.373,0.279,0.73,0.34,1.071h0.046c0.076-0.34,0.198-0.697,0.366-1.071l1.157-2.449h2.464l-2.19,3.951l2.368,3.673h-2.617l-1.341-2.529c-0.18-0.354-0.334-0.704-0.463-1.05h-0.033c-0.129,0.34-0.276,0.684-0.44,1.034l-1.378,2.545H19.141z"/></svg>'''
